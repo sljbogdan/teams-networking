fc94281a8b932a051492de0faeb33839e1d11b16
Salajan Bogdan <salajan.bogdan_a@yahoo.com>
2023-01-30 20:35:48 +0200

    add team with json

---
+++ function.js
@@ -0,0 +1,60 @@
+let allTeams = [];
+
+
+function loadTeams(){
+    fetch("data/teams.json")
+        .then(r => r.json())
+        .then(teams => {
+            console.warn("teams", teams)
+            allTeams = teams;
+            displayTeams(teams);
+        })
+}
+
+function getTeamsAsHTML(teams){
+    return teams.map(team => {
+        return `<tr>
+                <td>${team.group}</td>
+                <td>${team.members}</td>
+                <td>${team.name}</td>
+                <td>${team.url}</td>
+                <td>...</td>
+                </tr>`
+    }).join('');
+}
+
+function displayTeams(teams) {
+    const html = getTeamsAsHTML(teams);
+    document.querySelector('#table tbody').innerHTML = html;
+}
+
+function getTeamValues(){
+    
+    const group = document.querySelector('[name=group]').value;
+    const members = document.querySelector('[name=members]').value;
+    const name = document.querySelector('[name=name]').value;
+    const url = document.querySelector('[name=url]').value;
+    return  {
+        group: group,
+        members: members,
+        name,
+        url
+    };
+}
+
+function saveTeam(){
+    fetch("data/add-team.json")
+        .then(r => r.json())
+        .then(status => {
+            console.warn('status after add', status);
+        })
+}
+
+function submitTeam() {
+    const team = getTeamValues();
+    console.warn('add this value in teams.json', JSON.stringify(team));
+    saveTeam(team);
+    return false;
+}
+
+loadTeams();
